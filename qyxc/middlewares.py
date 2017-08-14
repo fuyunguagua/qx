7623cf8f336d8f7a1d7304a4712d883dd527ac6f
fuyunguagua <425006762@qq.com>
2017-08-14 10:24:01 +0800

    Use selenium to operate phantom

--- qyxc/middlewares.py
+++ qyxc/middlewares.py
@@ -1,9 +1,10 @@
 #coding=utf-8
 
 import random
-
-from scrapy.downloadermiddlewares.retry import RetryMiddleware
-from scrapy.downloadermiddlewares.redirect import RedirectMiddleware
+from scrapy.http import HtmlResponse
+import selenium
+from selenium import webdriver
+import time
 
 class RandomUserAgent(object):
     def __init__(self,agent):
@@ -15,8 +16,24 @@ class RandomUserAgent(object):
 
     def process_request(self,request,spider):
         request.headers.setdefault('User_Agent',random.choice(self.agents))
+#执行js代码使页面不断下拉，从而动态加载完成
+class CompleteRequste(object):
 
+    @classmethod
+    def process_request(self,request,spider):
+        if spider.name == 'qy_xingcheng_spider':
+            driver = webdriver.PhantomJS()#获得浏览器对象
+            driver.get(request.url)
 
+            def execute_times(times):
+                for i in range(times + 1):
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                    time.sleep(5)
+
+            execute_times(5)
+            content = driver.page_source.encode('utf-8')
+            driver.quit()
+            return HtmlResponse(request.url, encoding='utf-8', body=content, request=request)
 
 
 
