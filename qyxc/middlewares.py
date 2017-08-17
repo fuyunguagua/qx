6b083c7f014feb12da2244c86405045beed3f753
fuyunguagua <425006762@qq.com>
2017-08-17 17:20:41 +0800

    add proxy middlewares

--- qyxc/middlewares.py
+++ qyxc/middlewares.py
@@ -5,7 +5,7 @@ from scrapy.http import HtmlResponse
 import selenium
 from selenium import webdriver
 import time
-
+from qx.qyxc import settings
 class RandomUserAgent(object):
     def __init__(self,agent):
         self.agents = agent
@@ -18,7 +18,6 @@ class RandomUserAgent(object):
         request.headers.setdefault('User_Agent',random.choice(self.agents))
 #执行js代码使页面不断下拉，从而动态加载完成
 class CompleteRequste(object):
-
     @classmethod
     def process_request(self,request,spider):
         if spider.name == 'qy_xingcheng_spider':
@@ -36,8 +35,31 @@ class CompleteRequste(object):
             return HtmlResponse(request.url, encoding='utf-8', body=content, request=request)
 
 
+#设置代理
+class ProxyMiddleware(object):
+
+    def __init__(self):
+        self.proxys = settings.PROXY
+
+    def process_request(self,request,spider):
+        proxy = 'http://'+self.proxys[0]
+        #print(proxy)
+        request.meta['proxy'] = proxy
+
+    def process_response(self,request,response,spider):
+        print(response.url)
+        return response
+
+        #if response.status in [500,503,504,400,403,404,408]:
+         #   pass
+        #return response
 
 
+    def process_exception(self,request,exception,spider):
+        print(exception)
+        new_request = request.copy()
+        new_request.meta['proxy'] = 'http://'+random.choice(random.choice(self.proxys))
+        return new_request
 
 
 
